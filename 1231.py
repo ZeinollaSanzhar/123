935f699b0829faebe8d9d8b00168565ff1be8bbf
ZeinollaSanzhar <91206876+ZeinollaSanzhar@users.noreply.github.com>
2021-10-29 16:02:56 +0600

    Add files via upload

---
+++ 1231.py
@@ -0,0 +1,24 @@
+print ('Pyton work!')
+num=5
+print (num)
+print("Comment me :)")
+# print("Comment me :)")
+number = 2
+print(number ** 3)
+getsix = 1467 // 6
+print(getsix)
+string = "Data"
+mult = string * 4
+print(mult)
+if number % 2 == 0:
+    print("Yes")
+i = 4
+while i <= 13:
+   if i != 7 and i != 11:
+       print(i ** 2)
+
+   i += 1
+
+print(23 + 23 * 2)
+
+    
